4cb1a6d834a60f172f076c04a274c0e5d4e8d398
ekaxfrnd <ekaxfrnd@gmail.com>
2021-07-19 13:35:35 +0800

    fix partials

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ connectDB()
 
 // view engine
 const hbs = exphbs.create({
-    defaultLayout: 'main',
+    defaultLayout: 'dashboard',
     extname: 'hbs'
 })
 app.engine('hbs', hbs.engine)
-app.set('view engine', '.hbs')
+app.set('view engine', 'hbs')
 
 // static folder
 app.use(express.static(path.join(__dirname, 'assets')))
