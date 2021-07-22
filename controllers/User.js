0b8464a1b016e1da835e10b0c80b69a5e4266bee
ekaxfrnd <ekaxfrnd@gmail.com>
2021-07-22 21:53:50 +0800

    create controller for post register

--- controllers/User.js
+++ controllers/User.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const User = require('../models/User')
 
 module.exports = {
@@ -12,5 +14,41 @@ module.exports = {
             layout: 'auth',
             title: 'Login'
         })
+    },
+    postRegister: async (req, res) => {
+        try {
+            const {
+                first_name,
+                last_name,
+                email
+            } = req.body
+            let { password, confirm_password } = req.body
+            if(!first_name || !last_name || !email || !password || !confirm_password) {
+                res.redirect('/auth/register')
+                return
+            }
+            if(password != confirm_password) {
+                res.redirect('/auth/register')
+                return
+            }
+            const user = await User.findOne({ where: { email }})
+            if(!user) {
+                const hashedPassword = await bcrypt.hash(password, 12)
+                await User.create({
+                    first_name,
+                    last_name,
+                    email,
+                    password: hashedPassword
+                })
+                res.redirect('/auth/login')
+                return
+            } else {
+                res.redirect('/auth/register')
+                return
+            }
+        } catch (err) {
+            res.redirect('/auth/register')
+            return
+        }
     }
 }
