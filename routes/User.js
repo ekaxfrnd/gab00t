3289c76f7a4615960eee80e335df8386c51af1d1
ekaxfrnd <ekaxfrnd@gmail.com>
2021-07-22 22:00:02 +0800

    add post register handler

--- routes/User.js
+++ routes/User.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 
 const {
     getRegister,
-    getLogin
+    getLogin,
+    postRegister
 } = require('../controllers/User')
 
 router.get('/auth/register', getRegister)
 router.get('/auth/login', getLogin)
+router.post('/auth/register', postRegister)
 
 module.exports = router
